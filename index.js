113df224a3aa5fa258d0f214f97314dc5e45b5a1
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-03-07 08:16:53 -0300

    Vendo como funciona uma promessa assync

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ function trataErro(erro) {
   throw new Error(chalk.red(erro.code, "Não há arquivos no diretório"));
 }
 
+// async / await:
 function pegaArquivo(caminhoDoArquivo) {
   const encoding = "utf-8";
-  fs.promisses.readFile(caminhoDoArquivo, encoding)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch(trataErro);
+  const texto = fs.promises.readFile(caminhoDoArquivo, encoding);
+  console.log(texto);
 }
+
+// Promisses com then:
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = "utf-8";
+//   fs.promisses.readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErro);
+// }
 
 pegaArquivo("./TEXTO.txt");

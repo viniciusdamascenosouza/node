cb74359f9ed93dd8d49b8b281ce1923164f70714
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-06 08:09:54 -0300

    Tratando erros

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import fs from "fs";
 import chalk from "chalk";
 
+function trataErro (erro) {
+    throw new Error(chalk.bgRed(erro))
+}
+
 function pegaArquivo(caminhoDoArquivo) {
-  const encoding = "utf-8";
-  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    const encoding = "utf-8"
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro) {
+            trataErro(erro)
+        }
     console.log(chalk.green(texto));
   });
 }
 
+pegaArquivo("./arquivos/texto.m");
 pegaArquivo("./arquivos/texto.md");

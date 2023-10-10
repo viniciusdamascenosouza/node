08d94b086cb1b9c632cc05ad22805f07886c1c82
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-10 14:24:27 -0300

    Removendo funções sem uso, e comentando then()

--- index.js
+++ index.js
@@ -5,22 +5,13 @@ function trataErro(erro) {
   console.log(erro);
   throw new Error(chalk.bgRed(erro.code, "Não arquivo no diretório!"));
 }
+// PROMISES COM THEN()
 
 // function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = "utf-8"
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//         if(erro) {
-//             trataErro(erro)
-//         }
+//   const encoding = "utf-08";
+//   fs.promises.readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => {
 //     console.log(chalk.green(texto));
-//   });
+//   })
+//   .catch((erro) => trataErro(erro))
 // }
-
-function pegaArquivo(caminhoDoArquivo) {
-  const encoding = "utf-08";
-  fs.promises.readFile(caminhoDoArquivo, encoding)
-    .then((texto) => {
-    console.log(chalk.green(texto));
-  })
-  .catch((erro) => trataErro(erro))
-}

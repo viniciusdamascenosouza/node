cb195347ed88aaab3f0feda9da2483a4cb0b602e
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-10 14:39:00 -0300

    Mudando função para async/await

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ function trataErro(erro) {
   console.log(erro);
   throw new Error(chalk.bgRed(erro.code, "Não arquivo no diretório!"));
 }
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+  console.log(texto);
+}
+
+pegaArquivo("./arquivos/texto.md");
+
 // PROMISES COM THEN()
 
 // function pegaArquivo(caminhoDoArquivo) {

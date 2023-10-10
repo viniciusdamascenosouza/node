a00f417d3d8ce1347b73979fd15476a4ff94d51e
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-10 13:51:59 -0300

    Promessas

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 import fs from "fs";
 import chalk from "chalk";
 
-function trataErro (erro) {
-    throw new Error(chalk.bgRed(erro))
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.bgRed(erro.code, "Não arquivo no diretório!"));
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = "utf-8"
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if(erro) {
+//             trataErro(erro)
+//         }
+//     console.log(chalk.green(texto));
+//   });
+// }
 
 function pegaArquivo(caminhoDoArquivo) {
-    const encoding = "utf-8"
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if(erro) {
-            trataErro(erro)
-        }
+  const encoding = "utf-08";
+  fs.promises.readFile(caminhoDoArquivo, encoding)
+    .then((texto) => {
     console.log(chalk.green(texto));
-  });
+  })
+  .catch((erro) => trataErro(erro))
 }
-
-pegaArquivo("./arquivos/texto.m");
-pegaArquivo("./arquivos/texto.md");

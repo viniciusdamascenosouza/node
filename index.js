d83f55db548ddcb825dc8df1dfe831ba998564c1
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-11 10:34:20 -0300

    Limpaza de código

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 import fs from "fs";
 import chalk from "chalk";
 
-const textoTeste =
-  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).";
-
 function extraiLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
   const capturas = [...texto.matchAll(regex)];
 
-  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-  console.log(resultados);
+  const resultados = capturas.map((captura) => ({ [captura[1]]: captura[2] }));
+  return resultados;
 }
-
-extraiLinks(textoTeste);
 
 function trataErro(erro) {
   console.log(erro);
@@ -25,14 +20,14 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = "utf-8";
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(chalk.green(texto));
+    const executaExtraiLinks = extraiLinks(texto);
+    console.log(executaExtraiLinks);
   } catch (erro) {
     trataErro(erro);
   }
 }
 
-// pegaArquivo("./arquivos/texto.md");
-// pegaArquivo("./arquivos/");
+pegaArquivo("./arquivos/texto.md");
 
 // PROMISES COM THEN()
 
@@ -45,4 +40,4 @@ async function pegaArquivo(caminhoDoArquivo) {
 //   .catch((erro) => trataErro(erro))
 // }
 
-//              \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+//      expressão regular para extrair o link do texto.md        \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
